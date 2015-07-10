ca11ee963d2d2392f5f425e2d44b5be01d499993
waynegan <ganwei92@gmail.com>
2015-07-09 21:49:03 -0400

    make codes more readable.

--- CPD-4414-assignment3/src/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ CPD-4414-assignment3/src/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package org.netbeans.rest.application.config;
 
@@ -11,7 +7,7 @@ import javax.ws.rs.core.Application;
 
 /**
  *
- * @author Amanda Cohoon - c0628569
+ * @author wayne c0645457
  */
 @javax.ws.rs.ApplicationPath("")
 public class ApplicationConfig extends Application {
@@ -23,12 +19,7 @@ public class ApplicationConfig extends Application {
         return resources;
     }
 
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
+    
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(credentals.Product.class);
     }
